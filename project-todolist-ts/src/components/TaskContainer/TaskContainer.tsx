2abd2eab7b83f8c009001fa53f525b9b142c6d22
Maicon Souza <maicon_souza09@hotmail.com>
2022-10-25 19:03:07 -0300

    finished

--- project-todolist-ts/src/components/TaskContainer/TaskContainer.tsx
+++ project-todolist-ts/src/components/TaskContainer/TaskContainer.tsx
@@ -1,8 +1,30 @@
 import { NoTasks } from '../NoTasks/NoTasks'
+import { ViewTasks } from '../ViewTasks/ViewTasks'
 
 import styles from './TaskContainer.module.css'
 
-export function TaskContainer(){
+import { StructureTasks } from '../../App'
+import {  useState } from 'react'
+
+interface PropsTasks {
+    tasks: StructureTasks[];
+    concludedTask: (id:string) => void;
+    deletedTask: (id:string) => void;
+}
+
+
+export function TaskContainer({ tasks, concludedTask, deletedTask }:PropsTasks){
+
+    const createdTasks = tasks.map(e => e).length
+    const completed = tasks.filter(e => e.done).length
+
+
+
+    function transport(id:string, whatFunction:string){
+        whatFunction === 'concludedTask'? concludedTask(id) : deletedTask(id)
+    }
+
+    
 
     return (
         <div className={styles.container}>
@@ -10,18 +32,32 @@ export function TaskContainer(){
                 <p>
                     Tarefas criadas
                     <span>
-                        0
+                        {createdTasks}
                     </span>
                 </p>
                 <p>
                     Concluídas
                     <span>
-                        0
+                        {completed}
                     </span>
                 </p>
             </header>
             <main>
-                <NoTasks />
+                { createdTasks === 0 ? <NoTasks /> : null }
+                {
+                    tasks.map(e => {
+                        return (
+                            <ViewTasks 
+                                key={e.id} 
+                                id={e.id} 
+                                task={e.task} 
+                                done={e.done}
+                                transport={transport}
+                            />
+                        )
+                    })
+                }
+                
             </main>
         </div>
     )
